fd90c7e1c9c07a7891a85367a073800f562712a7
dhruvnakum <dhruv.nakum25@gmail.com>
2026-05-08 13:51:40 -0700

    fotmat convertpermtojwk

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import rsaPemToJwk from 'rsa-pem-to-jwk'
 
-const privateKey = fs.readFileSync("./certs/private.pem")
+const privateKey = fs.readFileSync('./certs/private.pem')
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-rsaPemToJwk(privateKey, {use: "sig"}, 'public')
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+rsaPemToJwk(privateKey, { use: 'sig' }, 'public')
