a6fee2102d6c8b503a1fa755a0262ceff28fd92a
dhruvnakum <dhruv.nakum25@gmail.com>
2026-06-23 13:29:53 -0700

    port change

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import cors from 'cors'
 const app = express()
 app.use(
     cors({
-        origin: ['http://localhost:5174'],
+        origin: ['http://localhost:5173'],
         credentials: true,
     }),
 )
